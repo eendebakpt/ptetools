6b88a5f3936b190311f09d449011cccbb786b8a6
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-01-18 20:28:47 +0100

    add tools

---
+++ src/ptetools/tools.py
@@ -0,0 +1,98 @@
+import os
+import tempfile
+import time
+from types import TracebackType
+from typing import Any, Literal
+
+
+class measure_time:
+    """Create context manager that measures execution time and prints to stdout
+
+    Example:
+        >>> import time
+        >>> with measure_time():
+        ...     time.sleep(.1)
+    """
+
+    def __init__(self, message: str | None = "dt: "):
+        self.message = message
+        self.dt = float("nan")
+
+    def __enter__(self) -> "measure_time":
+        self.start_time = time.perf_counter()
+        return self
+
+    @property
+    def current_delta_time(self) -> float:
+        """Return time since start of the context
+
+        Returns:
+            Time in seconds
+        """
+        return time.perf_counter() - self.start_time
+
+    @property
+    def delta_time(self) -> float:
+        """Return time spend in the context
+
+        If still in the context, return nan.
+
+        Returns:
+            Time in seconds
+        """
+        return self.dt
+
+    def __exit__(  # pylint: disable-all
+        self, exc_type: type[BaseException] | None, exc: BaseException | None, traceback: TracebackType | None
+    ) -> Literal[False]:
+        self.dt = time.perf_counter() - self.start_time
+
+        if self.message is not None:
+            print(f"{self.message} {self.dt:.3f} [s]")
+
+        return False
+
+
+def profile_expression(expression: str, N: int | None = 1, gui: str = "snakeviz") -> tuple[str, Any]:
+    """Profile an expression with cProfile and display the results using snakeviz
+
+    Args:
+        expression: Code to be profiled
+        N: Number of iterations. If None, then automatically determine a suitable number of iterations
+        gui: Can be `tuna` or `snakeviz`
+    Returns:
+        Tuple with the filename of the profiling results and a handle to the subprocess starting the GUI
+    """
+    import cProfile  # lazy import
+    import subprocess
+
+    tmpdir = tempfile.mkdtemp()
+    statsfile = os.path.join(tmpdir, "profile_expression_stats")
+
+    assert isinstance(expression, str), "expression should be a string"
+
+    if N is None:
+        t0 = time.perf_counter()
+        cProfile.run(expression, filename=statsfile)
+        dt = time.perf_counter() - t0
+        N = int(1.0 / max(dt - 0.6e-3, 1e-6))
+        if N <= 1:
+            print(f"profiling: 1 iteration, {dt:.2f} [s]")
+            r = subprocess.Popen([gui, statsfile])
+            return statsfile, r
+    else:
+        N = int(N)
+    print(f"profile_expression: running {N} loops")
+    if N > 1:
+        loop_expression = f"for ijk_kji_no_name in range({N}):\n"
+        loop_expression += "\n".join(["  " + term for term in expression.split("\n")])
+        loop_expression += "\n# loop done"
+        expression = loop_expression
+    t0 = time.perf_counter()
+    cProfile.run(expression, statsfile)
+    dt = time.perf_counter() - t0
+
+    print(f"profiling: {N} iterations, {dt:.2f} [s]")
+    r = subprocess.Popen([gui, statsfile])
+
+    return statsfile, r
