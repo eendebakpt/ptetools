d772521c05ed9e0f3d71441846e19c4a772beb06
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-02-06 11:51:46 +0100

    Add tests

---
+++ tests/test_qi.py
@@ -0,0 +1,16 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+
+from ptetools.qi import report_qi_status
+
+
+class TestQi(unittest.TestCase):
+    def test_report_qi_status(self):
+        with redirect_stdout(io.StringIO()) as s:
+            report_qi_status()
+        assert "QI backends" in s.getvalue()
+
+
+if __name__ == "__main__":
+    unittest.main()
