9c1df1b71359551c7793d7e30f4b38e34e66d22c
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-07-17 11:39:34 +0200

    fnamedtools

---
+++ tests/test_collections.py
@@ -0,0 +1,18 @@
+import unittest
+
+from IPython.lib.pretty import pretty
+
+from ptetools.collections import fnamedtuple
+
+
+def test_fnamedtuple():
+    Point = fnamedtuple("Point", ["x", "y"])
+    pt = Point(2, 3)
+
+    p = pretty(pt)
+
+    assert "Point" in p
+
+
+if __name__ == "__main__":
+    unittest.main()
