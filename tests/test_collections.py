8ac140d990d50de561ef5bc48de0d4f042713de0
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-07-17 11:40:12 +0200

    fnamedtools

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -5,13 +5,12 @@ from IPython.lib.pretty import pretty
 from ptetools.collections import fnamedtuple
 
 
-def test_fnamedtuple():
-    Point = fnamedtuple("Point", ["x", "y"])
-    pt = Point(2, 3)
-
-    p = pretty(pt)
-
-    assert "Point" in p
+class TestCollections(unittest.TestCase):
+    def test_fnamedtuple(self):
+        Point = fnamedtuple("Point", ["x", "y"])
+        pt = Point(2, 3)
+        p = pretty(pt)
+        assert "Point" in p
 
 
 if __name__ == "__main__":
