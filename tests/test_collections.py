66b8aabe64fcce1e95a326375c7a5f631c6ca59f
Pieter Eendebak <pieter.eendebak@gmail.com>
2026-01-27 17:40:21 +0100

    ci

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -7,20 +7,20 @@ from ptetools.collections import fnamedtuple
 
 class TestCollections(unittest.TestCase):
     def test_fnamedtuple(self):
-        Point = fnamedtuple("Point", ["x", "y"])
-        pt = Point(2, 3)
+        point = fnamedtuple("Point", ["x", "y"])
+        pt = point(2, 3)
         p = pretty(pt)
         assert "Point" in p
 
     def test_fnamedtuple_with_defaults(self):
-        Config = fnamedtuple("Config", ["name", "value"], defaults=["default"])
-        cfg = Config("test")
+        config = fnamedtuple("Config", ["name", "value"], defaults=["default"])
+        cfg = config("test")
         assert cfg.name == "test"
         assert cfg.value == "default"
 
     def test_fnamedtuple_access(self):
-        Vector = fnamedtuple("Vector", ["x", "y", "z"])
-        v = Vector(1, 2, 3)
+        vector = fnamedtuple("Vector", ["x", "y", "z"])
+        v = vector(1, 2, 3)
         assert v.x == 1
         assert v.y == 2
         assert v.z == 3
